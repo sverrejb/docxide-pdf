a51795eea1c50f00ec865afdd0d9c96e119a0979
Sverre Johann Bjørke <sverrejb@gmail.com>
2026-03-30 14:45:41 +0200

    Update lisa, annoations

--- lisa.sh
+++ lisa.sh
@@ -23,7 +23,7 @@ result=$(claude --permission-mode acceptEdits -p "@${progress} \
   You are to do the following steps:
 0. Read ./ralph/lisa_progress.md \
 1. Select from the top the first unsolved problem from tests/output/annotations.json. \
-2. Analyze the annotation, case input, output and reference pdf. Use the coordinates in the annotation to pinpoint the problem. Compare results and reference. Consult specs via local-rag mcp. Plan how to fix the problem and implement the plan. \
+2. Analyze the annotation, case input, output and reference pdf. Use the coordinates in the annotation to pinpoint the problem. Compare results and reference. Consult specs via local-rag mcp. Plan how to fix the problem and implement the plan. Do not defer work, or reason yourself out of doing it now. The only exception is systemic vertical text spacing or line length causing vertical shift.\
 3. If any meaningful improvement is achieved, and no substansial test regressions occur, commit the fix and write to the ./ralph/annotations_progress.md file, with an timestamp and reference to the problem that was fixed.
 4. If you believe the problem is fixed, set the fixed boolean in annotations.json for that case to true.
 5. Then output <promise>COMPLETE</promise>.")
